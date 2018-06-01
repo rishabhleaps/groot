e6e74a23ec56e9206c1e22b0a973f97f1afc7de1
rishabhleaps <rishabh@nineleaps.com>
2018-06-01 18:04:54 +0530

    project page designed and integrated

---
+++ app/app/components/projects/controllers/projectsController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('groot.controllers')
+  .controller('projectsController', ['$scope', '$state', '$stateParams','$timeout','localStorageService',
+    function($scope, $state, $stateParams,$timeout,localStorageService) {
+      function init() {
+      }
+
+      init();
+    }
+  ]);
