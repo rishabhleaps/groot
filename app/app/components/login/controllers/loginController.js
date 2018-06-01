5b6c30aacbe2a8fd74628a652bd6a9bec210f210
rishabhleaps <rishabh@nineleaps.com>
2018-06-01 21:49:02 +0530

    project detail and history page designed

--- app/app/components/login/controllers/loginController.js
+++ app/app/components/login/controllers/loginController.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('groot.controllers')
-  .controller('loginController', ['$scope', '$state', '$stateParams','localStorageService',
-    function($scope, $state, $stateParams,localStorageService) {
+  .controller('loginController', ['$scope', '$state', '$stateParams','growl','localStorageService',
+    function($scope, $state, $stateParams,growl,localStorageService) {
       function init() {
+        $scope.user = {};
       }
-      
+
+      $scope.login = function(){
+        if ($scope.user.email === 'admin@gmail.com' && $scope.user.password === 'admin'){
+          $state.go('projects');
+          localStorageService.set('name','Admin');
+        }else{
+          growl.error('Not so groot of us. Try Again!!!');
+        }
+      }
+
       init();
     }
   ]);
